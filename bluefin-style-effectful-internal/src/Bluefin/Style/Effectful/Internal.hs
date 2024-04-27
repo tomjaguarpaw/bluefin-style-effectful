0943dbf5785f00fb65143660d5ae8b24ce4a8cee
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-04-27 20:06:05 +0100

    WIP

--- bluefin-style-effectful-internal/src/Bluefin/Style/Effectful/Internal.hs
+++ bluefin-style-effectful-internal/src/Bluefin/Style/Effectful/Internal.hs
@@ -4,13 +4,16 @@ module Bluefin.Style.Effectful.Internal where
 
 import Control.Monad (when)
 import Data.Foldable (for_)
+import Data.Kind (Type)
 import Data.Void (Void, absurd)
-import Effectful.Error.Static (Error)
-import qualified Effectful.Error.Static as Eff
-import Effectful.State.Static.Local (State)
-import qualified Effectful.State.Static.Local as Eff
 import Effectful (Eff, runPureEff)
 import qualified Effectful as Eff
+import Effectful.Dispatch.Dynamic (interpret, localSeqLift, localSeqUnlift, reinterpret, send)
+import Effectful.Error.Static (Error)
+import qualified Effectful.Error.Static as Eff
+import Effectful.Fail (Fail)
+import Effectful.State.Static.Local (State)
+import qualified Effectful.State.Static.Local as Eff
 import Prelude hiding (return)
 
 class (a Eff.:> b) => a :> b
@@ -20,7 +23,7 @@ instance (e :> es) => e :> (x : es)
 instance {-# INCOHERENT #-} e :> (e : es)
 
 data Handle (eff :: k) (s :: k) where
-  MkHandle :: eff ~ s => Handle eff s
+  MkHandle :: (eff ~ s) => Handle eff s
 
 to :: ((eff :> es) => t es r) -> (s :> es) => Handle eff s -> t es r
 to = applyHandle
@@ -79,3 +82,82 @@ twoState = runPureEff $
       s1' <- get s1
       s2' <- get s2
       pure (s1', s2')
+
+-- Direct encoding of the effects we want in our filesystem
+data Filesystem m = MkFilesystem
+  { readFileFS :: FilePath -> m String,
+    writeFileFS :: FilePath -> String -> m ()
+  }
+
+-- A type class for such direct-style effects
+class EffectDirect (e :: (Type -> Type) -> Type) where
+  fmapEffect :: (forall a. m a -> n a) -> e m -> e n
+
+-- Instance for the filesystem effect
+instance EffectDirect Filesystem where
+  fmapEffect f MkFilesystem {readFileFS = r, writeFileFS = w} =
+    MkFilesystem {readFileFS = f . r, writeFileFS = \s -> f . w s}
+
+-- Adapt direct effects for the existing cleff type-level encoding
+data Direct f (m :: Type -> Type) (a :: Type) = Direct (f m -> m a)
+
+type instance Eff.DispatchOf (Direct f) = Eff.Dynamic
+
+-- Direct version of send
+sendDirect ::
+  Direct f :> es => (f (Eff es) -> Eff es a) -> Eff es a
+sendDirect = send . Direct
+
+-- Define the effectful operations using sendDirect
+readFile' ::
+  (Direct Filesystem :> es) => FilePath -> Eff es String
+readFile' x = sendDirect (\f -> readFileFS f x)
+
+writeFile' ::
+  (Direct Filesystem :> es) => FilePath -> String -> Eff es ()
+writeFile' x y = sendDirect (\f -> writeFileFS f x y)
+
+-- To run in IO, just define the effects directly
+runFilesystemIO ::
+  (Eff.IOE :> es) =>
+  Eff (Direct Filesystem : es) a ->
+  Eff es a
+runFilesystemIO =
+  interpretDirectIO
+    MkFilesystem
+      { readFileFS = readFile,
+        writeFileFS = writeFile
+      }
+
+-- To run in State, just define the effects directly
+filesystemToState ::
+  (Fail :> es, State [(FilePath, String)] Eff.:> es) =>
+  Eff (Direct Filesystem : es) a ->
+  Eff es a
+filesystemToState =
+  interpretDirect
+    MkFilesystem
+      { readFileFS = \path ->
+          Eff.gets (lookup path) >>= \case
+            Nothing -> fail ("File not found: " ++ show path)
+            Just contents -> pure contents,
+        writeFileFS = \path contents ->
+          Eff.modify ((path, contents) :)
+      }
+
+interpretDirectIO ::
+  (Eff.IOE :> es, EffectDirect f) =>
+  f IO ->
+  Eff (Direct f : es) a ->
+  Eff es a
+interpretDirectIO = interpretDirect . fmapEffect Eff.liftIO
+
+interpretDirect ::
+  (EffectDirect f) =>
+  f (Eff es) ->
+  Eff (Direct f : es) a ->
+  Eff es a
+interpretDirect p = interpret $ \env (Direct f) ->
+  localSeqUnlift env $ \k' ->
+    localSeqLift env $ \k ->
+      k' (f (fmapEffect k p))
